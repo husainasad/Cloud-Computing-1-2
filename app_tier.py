a45b9ab03a0ea04b9d71fc0d6aba10073a63c6bc
husainasad <asad1999@gmail.com>
2024-03-07 19:19:31 -0700

    implemented request-response SQS

--- app_tier.py
+++ app_tier.py
@@ -1,23 +1,76 @@
 from Resources.model.face_recognition import face_match
-from fastapi import FastAPI, UploadFile
-from fastapi.responses import PlainTextResponse
+import boto3, base64, json, asyncio
 
-app = FastAPI()
+AWS_REGION = 'us-east-1'
+REQUEST_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-req-queue'
+RESPONSE_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-resp-queue'
+
+sqs_client = boto3.client('sqs', region_name=AWS_REGION)
 
 data_pt_path = './Resources/model/data.pt'
 
-async def process_img(input_file):
+async def process_img(encoded_img):
     try:
-        img_content = await input_file.read()
-        with open("temp_image.jpg", "wb") as f:
-            f.write(img_content)
-        result = face_match("temp_image.jpg", data_pt_path)[0]
-        img_file_name = input_file.filename.split('.')[0]
-        return f"{img_file_name}:{result}"
+        image_data = base64.b64decode(encoded_img)
+        with open('temp.jpg', 'wb') as f:
+            f.write(image_data)
+        result = face_match('temp.jpg', data_pt_path)[0]
+        return f'{result}'
     except Exception as e:
         return f"Error: {str(e)}"
+    
 
-@app.post("/", response_class=PlainTextResponse)
-async def get_img_result(inputFile: UploadFile):
-    output = await process_img(inputFile)
-    return output
+async def process_msg():
+    while True:
+        receive_response = sqs_client.receive_message(
+            QueueUrl=REQUEST_QUEUE_URL,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+
+        if 'Messages' in receive_response:
+            message = receive_response['Messages'][0]
+            
+            message_body = json.loads(message['Body'])
+
+            image_name = message_body['image_name']
+            # print('image name is: ' + image_name)
+
+            image_encoded = message_body['image_encoded']
+
+            # print('Received image data from SQS')
+
+            model_result = await process_img(image_encoded)
+
+            # Delete received message from queue
+            receipt_handle = message['ReceiptHandle']
+            sqs_client.delete_message(
+                QueueUrl=REQUEST_QUEUE_URL,
+                ReceiptHandle=receipt_handle
+            )
+            # print('Deleted message')
+            
+            output_msg = f'{image_name}:{model_result}'
+            # print(output_msg)
+
+            send_response = sqs_client.send_message(
+                QueueUrl=RESPONSE_QUEUE_URL,
+                MessageBody=(
+                    output_msg
+                )
+            )
+
+            # print(f"Message sent to Response SQS with MessageId: {send_response['MessageId']}")
+
+
+if __name__ == "__main__":
+    # process_msg()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(process_msg())
--- web_tier.py
+++ web_tier.py
@@ -1,21 +1,65 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import PlainTextResponse
-import httpx
+import boto3
+import base64
+import json
 
 app = FastAPI()
 
-APP_URL = 'http://127.0.0.1:5000/'
+AWS_REGION = 'us-east-1'
+REQUEST_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-req-queue'
+RESPONSE_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-resp-queue'
 
-# async def fetch_app(input_file):
-#     async with httpx.AsyncClient() as client:
-#         app_response = await client.post(APP_URL, files={"inputFile": (input_file.filename, input_file.file)})
-#     return app_response.text
+sqs_client = boto3.client('sqs', region_name=AWS_REGION)
 
 @app.post("/", response_class=PlainTextResponse)
 async def get_app_result(inputFile: UploadFile):
-    # app_result = await fetch_app(inputFile)
+    
+    file_content = await inputFile.read()
+    image_name = inputFile.filename.split('.')[0]
+    image_encoded = base64.b64encode(file_content).decode('utf-8')
 
-    async with httpx.AsyncClient() as client:
-        app_result = await client.post(APP_URL, files={"inputFile": (inputFile.filename, inputFile.file)})
-    # return f"{app_result}"
-    return app_result.text
+    message = {
+        'image_name': image_name,
+        'image_encoded': image_encoded
+    }
+
+    message_json = json.dumps(message)
+
+    send_response = sqs_client.send_message(
+        QueueUrl=REQUEST_QUEUE_URL,
+        MessageBody= message_json
+    )
+
+    # print(f"Message sent to Request SQS with MessageId: {send_response['MessageId']}")
+
+    receive_response = sqs_client.receive_message(
+        QueueUrl=RESPONSE_QUEUE_URL,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=20
+    )
+
+    if 'Messages' in receive_response:
+        message = receive_response['Messages'][0]
+
+        app_result = message['Body']
+
+        # print('Received image output from SQS')
+
+        # Delete received message from queue
+        receipt_handle = message['ReceiptHandle']
+        sqs_client.delete_message(
+            QueueUrl=RESPONSE_QUEUE_URL,
+            ReceiptHandle=receipt_handle
+        )
+        # print('Deleted message')
+        
+        # print(app_result)
+        return app_result
