898e82b13e2e68797c1798746ce17edf13984ac8
husainasad <asad1999@gmail.com>
2024-03-10 15:29:19 -0700

    implemented asyncio to decouple request and response tasks

--- web_tier.py
+++ web_tier.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import PlainTextResponse
-import boto3
-import base64
-import json
+import boto3, base64, json, asyncio
 
 app = FastAPI()
 
@@ -10,19 +8,13 @@ AWS_REGION = 'us-east-1'
 REQUEST_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-req-queue'
 RESPONSE_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-resp-queue'
 
-# sqs_client = boto3.client('sqs', region_name=AWS_REGION)
-
 sts_client = boto3.client('sts')
 
 session = boto3.Session()
 
 sqs_client = session.client('sqs', region_name=AWS_REGION)
-
-@app.post("/", response_class=PlainTextResponse)
-async def get_app_result(inputFile: UploadFile):
     
-    file_content = await inputFile.read()
-    image_name = inputFile.filename.split('.')[0]
+async def process_request(file_content, image_name):
     image_encoded = base64.b64encode(file_content).decode('utf-8')
 
     message = {
@@ -32,13 +24,12 @@ async def get_app_result(inputFile: UploadFile):
 
     message_json = json.dumps(message)
 
-    send_response = sqs_client.send_message(
+    sqs_client.send_message(
         QueueUrl=REQUEST_QUEUE_URL,
-        MessageBody= message_json
+        MessageBody=message_json
     )
 
-    # print(f"Message sent to Request SQS with MessageId: {send_response['MessageId']}")
-
+async def process_response():
     receive_response = sqs_client.receive_message(
         QueueUrl=RESPONSE_QUEUE_URL,
         AttributeNames=[
@@ -57,15 +48,25 @@ async def get_app_result(inputFile: UploadFile):
 
         app_result = message['Body']
 
-        # print('Received image output from SQS')
-
-        # Delete received message from queue
         receipt_handle = message['ReceiptHandle']
         sqs_client.delete_message(
             QueueUrl=RESPONSE_QUEUE_URL,
             ReceiptHandle=receipt_handle
         )
-        # print('Deleted message')
-        
-        # print(app_result)
         return app_result
+
+@app.post("/", response_class=PlainTextResponse)
+async def get_app_result(inputFile: UploadFile):
+    
+    file_content = await inputFile.read()
+    image_name = inputFile.filename.split('.')[0]
+
+    request_task = asyncio.ensure_future(process_request(file_content, image_name))
+
+    response_task = asyncio.ensure_future(process_response())
+
+    await asyncio.gather(request_task, response_task)
+
+    response = response_task.result()
+
+    return response
