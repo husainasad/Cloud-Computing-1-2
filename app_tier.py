8bfb79ffcfaa0e823f2dd3e73abbcac42ed6d921
husainasad <asad1999@gmail.com>
2024-03-10 15:25:50 -0700

    implemented add request/response to S3 buckets

--- app_tier.py
+++ app_tier.py
@@ -4,25 +4,22 @@ import boto3, base64, json, asyncio
 AWS_REGION = 'us-east-1'
 REQUEST_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-req-queue'
 RESPONSE_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/223420481310/1225380117-resp-queue'
-
-# sqs_client = boto3.client('sqs', region_name=AWS_REGION)
+IN_BUCKET = '1225380117-in-bucket'
+OUT_BUCKET = '1225380117-out-bucket'
 
 sts_client = boto3.client('sts')
-
-# response = sts_client.get_caller_identity()
-# instance_role_name = response['Arn'].split('/')[1]
-
-# print(instance_role_name)
 
 session = boto3.Session()
 
 sqs_client = session.client('sqs', region_name=AWS_REGION)
 
+s3_client = session.client('s3')
+
 data_pt_path = './Resources/model/data.pt'
 
-async def process_img(encoded_img):
+async def process_img(image_data):
     try:
-        image_data = base64.b64decode(encoded_img)
+        # image_data = base64.b64decode(encoded_img)
         with open('temp.jpg', 'wb') as f:
             f.write(image_data)
         result = face_match('temp.jpg', data_pt_path)[0]
@@ -55,10 +52,15 @@ async def process_msg():
             # print('image name is: ' + image_name)
 
             image_encoded = message_body['image_encoded']
+            image_data = base64.b64decode(image_encoded)
 
             # print('Received image data from SQS')
 
-            model_result = await process_img(image_encoded)
+            model_result = await process_img(image_data)
+
+            # Add request and response in S3 buckets
+            s3_client.put_object(Body=image_data, Bucket=IN_BUCKET, Key=image_name + '.jpg')
+            s3_client.put_object(Body=model_result, Bucket=OUT_BUCKET, Key=image_name)
 
             # Delete received message from queue
             receipt_handle = message['ReceiptHandle']
@@ -71,7 +73,7 @@ async def process_msg():
             output_msg = f'{image_name}:{model_result}'
             # print(output_msg)
 
-            send_response = sqs_client.send_message(
+            sqs_client.send_message(
                 QueueUrl=RESPONSE_QUEUE_URL,
                 MessageBody=(
                     output_msg
